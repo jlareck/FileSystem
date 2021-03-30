80fba8523cf724c49b2b5f07d915e77d66cb375b
jlareck <medik2507@gmail.com>
2021-03-31 00:54:15 +0300

    fixed open file table and implemented file descriptor

--- src/main/java/com/fs/filesystem/OpenFileTable.java
+++ src/main/java/com/fs/filesystem/OpenFileTable.java
@@ -1,6 +1,11 @@
 package com.fs.filesystem;
 
+import com.fs.utils.FileSystemConfig;
+
 public class OpenFileTable {
+    OpenFileTableEntry[] entries;
 
-
+    public OpenFileTable() {
+        entries = new OpenFileTableEntry[FileSystemConfig.OFT_NUMBER_OF_ENTRIES];
+    }
 }
