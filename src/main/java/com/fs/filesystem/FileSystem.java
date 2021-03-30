02539df5962acf7c24406cc2b036a084e3176c76
jlareck <medik2507@gmail.com>
2021-03-31 02:29:26 +0300

    fixed bitmap

--- src/main/java/com/fs/filesystem/FileSystem.java
+++ src/main/java/com/fs/filesystem/FileSystem.java
@@ -3,14 +3,18 @@ package com.fs.filesystem;
 import com.fs.iosystem.IOSystem;
 import com.fs.ldisk.LDisk;
 
+import java.util.BitSet;
+
 public class FileSystem {
 
     public IOSystem ioSystem;
-    public byte[] bitmap;
+    public BitSet bitmap;
 
     public FileSystem(IOSystem ioSystem) {
         this.ioSystem = ioSystem;
-        bitmap = new byte[LDisk.BLOCKS_AMOUNT];
+        // bitmap setting true: 1 for bitmap, 6 for descriptors, 3 for directory
+        bitmap = new BitSet(64);
+        bitmap.set(0,10,true);
     }
 
 }
