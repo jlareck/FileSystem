e9d525fb865db3665e7f5d4286a31484e89f03b9
TaisiiaFenz <51312876+TaisiiaFenz@users.noreply.github.com>
2021-04-07 14:06:04 +0300

    create interface of shell

---
+++ src/main/java/com/fs/shell/Shell.java
@@ -0,0 +1,7 @@
+package com.fs.shell;
+
+import com.fs.iosystem.IOSystem;
+
+public class Shell {
+
+}
---
+++ src/main/java/com/fs/shell/ShellView.java
@@ -0,0 +1,16 @@
+package com.fs.shell;
+
+public interface ShellView {
+
+    void begin();
+    void create(String fileName);
+    void destroy(String fileName);
+    void open(String fileName);
+    void close(int index);
+    void read(int index, int count);
+    void write(int index, char c, int count);
+    void seek(int index, int pos);
+    void directory();
+    void init(String diskCont); //parameters
+    void save(String diskCont);
+}
